1f89817012937fa58903189bab53dd5c82e41ee1
Dariusz Gulbicki <kontakt@dariuszgulbicki.pl>
2025-04-10 03:38:48 +0200

    Updated test to new model

--- Tests/LoggingCampTests/LoggingCampTests.swift
+++ Tests/LoggingCampTests/LoggingCampTests.swift
@@ -8,7 +8,7 @@ final class LoggingCampTests: XCTestCase {
         let logger = Logger("test")
         logger.setEnabledHandlers(["default"])
 
-        XCTAssertEqual(logger.getName(), "test")
+        XCTAssertEqual(logger.getId(), "test")
         XCTAssertEqual(logger.getEnabledHandlers(), ["default"])
 
         logger.debug("This is a debug message")
@@ -20,12 +20,12 @@ final class LoggingCampTests: XCTestCase {
 
     func testLoggingPool() throws {
         let logger = Logger("test")
-        let loggingPool = LoggerPool()
+        let loggingPool = LoggerPool("new")
 
         loggingPool.addLogger(logger)
 
         XCTAssertEqual(loggingPool.getLoggers().count, 1)
-        XCTAssertEqual(loggingPool["test"].getName(), "test")
+        XCTAssertEqual(loggingPool["test"].getId(), "test")
 
         loggingPool["newLogger"].info("New logger created")
 
